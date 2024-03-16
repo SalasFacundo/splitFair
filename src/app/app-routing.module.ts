ab00205b33a0285aca3bf262f316c844efc21f19
Facundo <salasfacundo96@gmail.com>
2024-03-16 19:52:47 -0300

    desarrollando pagina nuevo grupo

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,9 +1,10 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { OldGroupsComponent } from './pages/old-groups/old-groups.component';
+import { NewGroupComponent } from './pages/new-group/new-group.component';
 
 const routes: Routes = [
-  { path: '', component: OldGroupsComponent },
+  { path: '', component: NewGroupComponent },
   //{ path: 'scores', component: ScoreListComponent }
 ];
 
--- src/app/pages/pages.module.ts
+++ src/app/pages/pages.module.ts
@@ -4,17 +4,29 @@ import {MatDividerModule} from '@angular/material/divider';
 import { OldGroupsComponent } from './old-groups/old-groups.component';
 import { ComponentsModule } from '../components/components.module';
 import {MatIconModule} from '@angular/material/icon';
+import { NewGroupComponent } from './new-group/new-group.component';
+import {MatExpansionModule} from '@angular/material/expansion';
+import {MatDatepickerModule} from '@angular/material/datepicker';
+import {MatInputModule} from '@angular/material/input';
+import {MatFormFieldModule} from '@angular/material/form-field';
+import {MatButtonModule} from '@angular/material/button';
 
 
 @NgModule({
   declarations: [
-    OldGroupsComponent
+    OldGroupsComponent,
+    NewGroupComponent
 ],
   imports: [
     CommonModule,
     MatDividerModule,
     ComponentsModule,
-    MatIconModule
+    MatIconModule,
+    MatExpansionModule,
+    MatDatepickerModule,
+    MatInputModule,
+    MatFormFieldModule,
+    MatButtonModule
   ],
   exports: [
     OldGroupsComponent
